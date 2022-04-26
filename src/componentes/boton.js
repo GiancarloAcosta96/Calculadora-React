dd5b254d8754a6e0fdffbe83421657123cca4e2f
Gian <35286472+GiancarloAcosta96@users.noreply.github.com>
2022-04-26 01:55:38 -0500

    comparacion de tipo de dato

--- src/componentes/boton.js
+++ src/componentes/boton.js
@@ -4,7 +4,7 @@ import '../estilos/boton.css'
 export default function Boton(props) {
 
     const esOperador = valor => {
-        return isNaN(valor) && (valor != '.') && (valor != '=');
+        return isNaN(valor) && (valor !== '.') && (valor !== '=');
     }
 
     return(
